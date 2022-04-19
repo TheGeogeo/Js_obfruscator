40ee67f1a6ad8b0c20ccbe62b74ee92c0807c0c3
TheGeogeo <trolface87@hotmail.fr>
2022-04-19 21:17:21 +0200

    up

--- startObfuscate.js
+++ startObfuscate.js
@@ -18,14 +18,70 @@ if (files.length > 0) {
         fs.readFile(notObf + name, 'utf-8', (error, code) => {
             if (error) return console.log(error);
 
-            let options = {
+            const options = {
                 mangle: {
                     toplevel: true,
                 },
                 nameCache: {}
             }
             let minify = UglifyJS.minify(code, options);
-            let obfuscat = jsObfuscator.obfuscate(minify.code);
+
+            const option = {
+                compact: true,
+                controlFlowFlattening: false,
+                controlFlowFlatteningThreshold: 0.75,
+                deadCodeInjection: true,
+                deadCodeInjectionThreshold: 5,
+                debugProtection: false,
+                debugProtectionInterval: 0,
+                disableConsoleOutput: false,
+                domainLock: [],
+                domainLockRedirectUrl: 'about:blank',
+                forceTransformStrings: [],
+                identifierNamesCache: null,
+                identifierNamesGenerator: 'hexadecimal',
+                identifiersDictionary: [],
+                identifiersPrefix: '',
+                ignoreRequireImports: false,
+                inputFileName: '',
+                log: false,
+                numbersToExpressions: false,
+                optionsPreset: 'default',
+                renameGlobals: false,
+                renameProperties: false,
+                renamePropertiesMode: 'safe',
+                reservedNames: [],
+                reservedStrings: [],
+                seed: 0,
+                selfDefending: false,
+                simplify: true,
+                sourceMap: false,
+                sourceMapBaseUrl: '',
+                sourceMapFileName: '',
+                sourceMapMode: 'separate',
+                sourceMapSourcesMode: 'sources-content',
+                splitStrings: true,
+                splitStringsChunkLength: 10,
+                stringArray: true,
+                stringArrayCallsTransform: true,
+                stringArrayCallsTransformThreshold: 0.5,
+                stringArrayEncoding: ['rc4'],
+                stringArrayIndexesType: [
+                    'hexadecimal-number'
+                ],
+                stringArrayIndexShift: true,
+                stringArrayRotate: true,
+                stringArrayShuffle: true,
+                stringArrayWrappersCount: 1,
+                stringArrayWrappersChainedCalls: true,
+                stringArrayWrappersParametersMaxCount: 2,
+                stringArrayWrappersType: 'variable',
+                stringArrayThreshold: 0.75,
+                target: 'browser',
+                transformObjectKeys: false,
+                unicodeEscapeSequence: false
+            }
+            let obfuscat = jsObfuscator.obfuscate(minify.code, option);
 
             fs.writeFile(Obf + name, obfuscat.getObfuscatedCode(), (err) => {
                 if (err) return console.log(err);
